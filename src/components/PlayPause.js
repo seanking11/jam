80eb41220920905ff0cdd1a72d4d9e3ab3038600
Sean King <kingsc@miamioh.edu>
2020-01-27 22:55:07 -0800

    Actually add PausePlay component

---
+++ src/components/PlayPause.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+const PlayPause = ({ onToggle = () => {} }) => {
+    const [isPlaying, setPlaying] = useState(false)
+    return (
+        <div>
+            {isPlaying ? (
+                <span
+                    role="img"
+                    aria-label="X"
+                    onClick={() => {
+                        onToggle()
+                        setPlaying(false)
+                    }}
+                >
+                    ⏸️
+                </span>
+            ) : (
+                <span
+                    role="img"
+                    aria-label="X"
+                    onClick={() => {
+                        onToggle()
+                        setPlaying(true)
+                    }}
+                >
+                    ▶️
+                </span>
+            )}
+        </div>
+    )
+}
+
+export default PlayPause
