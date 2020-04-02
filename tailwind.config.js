5a88ee441f7b908a107f574ad6a129b29a6fc702
Sean King <kingsc@miamioh.edu>
2020-04-01 23:54:41 -0700

    Add light and dark themes and hookup to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    theme: {
+        colors: {},
+        textColor: {
+            primary: 'var(--color-text-primary)',
+            secondary: 'var(--color-text-secondary)',
+            danger: 'var(--color-text-danger)',
+            default: 'var(--color-text-default)',
+            'default-soft': 'var(--color-text-default-soft)',
+            inverse: 'var(--color-text-inverse)',
+            'inverse-soft': 'var(--color-text-inverse-soft)',
+        },
+        backgroundColor: {
+            primary: 'var(--color-bg-primary)',
+            secondary: 'var(--color-bg-secondary)',
+            danger: 'var(--color-bg-danger)',
+            default: 'var(--color-bg-default)',
+            'default-soft': 'var(--color-bg-default-soft)',
+            inverse: 'var(--color-bg-inverse)',
+            'inverse-soft': 'var(--color-bg-inverse-soft)',
+        },
+    },
+    variants: {},
+    plugins: [],
+}
