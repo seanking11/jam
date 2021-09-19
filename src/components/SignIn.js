c1b4150f2d0f4cd9e7603f13829dcd93e482189a
sean <kingsc@miamioh.edu>
2021-09-18 21:54:26 -0700

    Make logins look nice

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -20,25 +20,59 @@ const SignIn = ({ toggleVisibility, setUser, history }) => {
     }
 
     return (
-        <div>
-            <h4>Sign In</h4>
-            <input
-                type="text"
-                className="text-black"
-                placeholder="guitarguy69@gmail.com"
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-                type="password"
-                className="text-black"
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={handleSignIn}>Sign In</button>
-            <div onClick={() => toggleVisibility()}>Need an account?</div>
+        <div className="flex h-screen">
+            <div className="p-10 s:p-0 m-auto md:w-full md:max-w-md">
+                <h1 className="font-bold text-center text-2xl mb-5">Login</h1>
+                <div className="bg-default-hard shadow w-full rounded-lg divide-y divide-gray-200">
+                    <div className="px-5 py-7">
+                        <label className="font-semibold text-sm text-gray-100 pb-1 block">
+                            E-mail
+                        </label>
+                        <input
+                            type="text"
+                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full text-white bg-default "
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label className="font-semibold text-sm text-gray-100 pb-1 block">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full text-white bg-default"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button
+                            type="button"
+                            className="transition duration-200 bg-primary hover:bg-primary-soft text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+                            onClick={handleSignIn}
+                        >
+                            <span className="inline-block mr-2">Login</span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                className="w-4 h-4 inline-block"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                />
+                            </svg>
+                        </button>
+                        <div
+                            onClick={() => toggleVisibility()}
+                            className="italic text-sm text-center text-gray-100 mt-2 cursor-pointer"
+                        >
+                            Need an account?
+                        </div>
 
-            <hr />
-
-            {error && <div>{error}</div>}
+                        {error && <div>{error}</div>}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -19,30 +19,63 @@ const SignUp = ({ toggleVisibility, history, setUser }) => {
     }
 
     return (
-        <div>
-            <h4>Sign Up</h4>
-            {/* <form id='sign-up'> */}
-            <input
-                type="text"
-                className="text-black"
-                placeholder="guitarguy69@gmail.com"
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-                type="password"
-                className="text-black"
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            {/* <button onSubmit={handleSignUp} form='sign-up'>Sign Up!</button> */}
-            <button onClick={handleSignUp}>Sign Up!</button>
-            {/* </form> */}
-            <div onClick={() => toggleVisibility()}>
-                Already have an account?
+        <div className="flex h-screen">
+            <div className="p-10 s:p-0 m-auto md:w-full md:max-w-md">
+                <h1 className="font-bold text-center text-2xl mb-5">
+                    Create an account
+                </h1>
+                <div className="bg-default-hard shadow w-full rounded-lg divide-y divide-gray-200">
+                    <div className="px-5 py-7">
+                        <label className="font-semibold text-sm text-gray-100 pb-1 block">
+                            E-mail
+                        </label>
+                        <input
+                            type="text"
+                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full text-white bg-default "
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <label className="font-semibold text-sm text-gray-100 pb-1 block">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            className="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full text-white bg-default"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button
+                            type="button"
+                            className="transition duration-200 bg-primary hover:bg-primary-soft text-white w-full py-2.5 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+                            onClick={handleSignUp}
+                        >
+                            <span className="inline-block mr-2">
+                                Create an account
+                            </span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                className="w-4 h-4 inline-block"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                                />
+                            </svg>
+                        </button>
+                        <div
+                            onClick={() => toggleVisibility()}
+                            className="italic text-sm text-center text-gray-100 mt-2 cursor-pointer"
+                        >
+                            Already have an account?
+                        </div>
+
+                        {error && <div>{error}</div>}
+                    </div>
+                </div>
             </div>
-
-            <hr />
-
-            {error && <div>{error}</div>}
         </div>
     )
 }
